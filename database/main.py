f83f6d098f841c704efae50185483bb7286dfdd5
aksnirp <mprinska19@gmail.com>
2025-01-27 20:56:52 +0545

    main.py file rearrange to main root directory

--- database/main.py
+++
@@ -1,8 +0,0 @@
-from setup import init_db
-from utilities import add_user
-
-def main():
-    init_db()
-
-    
-main()
---
+++ main.py
@@ -0,0 +1,8 @@
+from database.financialadvisor.financialadvisor.db_setup import init_db
+#from utilities import add_user
+
+def main():
+    init_db()
+
+    
+main()
